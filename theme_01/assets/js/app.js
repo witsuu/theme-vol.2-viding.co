746ba1d6a5b738e2340e99b21e94251c7a54a43b
witsuu <witsudi217@gmail.com>
2021-12-06 16:28:29 +0700

    update

---
+++ theme_01/assets/js/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+$(document).ready(() => {
+  $("#auto-scroll").infiniteslide({
+    speed: 20,
+    direction: "up",
+  });
+});
---
+++ theme_02/assets/js/app.js
@@ -0,0 +1,30 @@
+"use strict";
+
+$(document).ready(() => {
+  const width = window.screen.width;
+  let item;
+  if (width >= 576) {
+    item = 3;
+  } else {
+    item = 2;
+  }
+
+  const owl = $(".owl-carousel");
+  owl.owlCarousel({
+    items: item,
+    loop: true,
+    dots: false,
+  });
+
+  $(".next").on("click", () => {
+    owl.trigger("next.owl.carousel");
+  });
+  $(".prev").on("click", () => {
+    owl.trigger("prev.owl.carousel");
+  });
+
+  $("#auto-scroll").infiniteslide({
+    speed: 20,
+    direction: "up",
+  });
+});
