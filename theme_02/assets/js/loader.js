fb45d095ad911f173fafc8548df53d68d93b2128
witsuu <witsudi217@gmail.com>
2021-12-28 19:48:00 +0700

    fixed animation

--- theme_02/assets/js/loader.js
+++ theme_02/assets/js/loader.js
@@ -1,18 +1,9 @@
-// document.onreadystatechange = () => {
-//   if (document.readyState !== "complete") {
-//     document.querySelector(".main-app").style.display = "none";
-//     document.querySelector(".loader-wrapper").style.display = "block";
-//   } else {
-//     document.querySelector(".main-app").style.display = "block";
-//     document.querySelector(".loader-wrapper").style.display = "none";
-//   }
-// };
-$(document).on("readystatechange", () => {
+document.onreadystatechange = () => {
   if (document.readyState !== "complete") {
-    $(".main-app").hide();
-    $(".load-wrapper").show();
+    document.querySelector(".main-app").style.display = "none";
+    document.querySelector(".load-wrapper").style.display = "block";
   } else {
-    $(".main-app").show();
-    $(".load-wrapper").hide();
+    document.querySelector(".main-app").style.display = "block";
+    document.querySelector(".load-wrapper").style.display = "none";
   }
-});
+};
